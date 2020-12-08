6ea6f9ab047efab5e77110cca20bef957c5d9d43
yuxuan wang <2879296442@qq.com>
2020-12-08 21:34:48 +0000

    Delete movie - valid test case

--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -1,5 +1,6 @@
 import chai from "chai";
 import request from "supertest";
+import {moviesObject} from '../../../../api/movies/movies';
 let api ;
 
 const expect = chai.expect;
@@ -40,9 +41,52 @@ const movie = {
   vote_average: 7,
   vote_count: 9692
 };
-
 describe("Movies endpoint", () => {
   beforeEach(() => {
+    while (moviesObject.movies.length > 0) {
+      moviesObject.movies.pop()
+    }
+    moviesObject.movies.push(
+      {
+        "title": "The Avengers",
+        "poster_path": "/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
+        "overview": "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
+        "release_date": "2012-04-25",
+        "genre_ids": [
+          878,
+          28,
+          12
+        ],
+        "id": 24428,
+        "original_title": "The Avengers",
+        "original_language": "en",
+        "backdrop_path": "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg",
+        "popularity": 7.353212,
+        "vote_count": 8503,
+        "video": false,
+        "vote_average": 7.33
+      } )
+    moviesObject.movies.push(
+      {
+        "title": "Avengers: Age of Ultron",
+        "poster_path": "/t90Y3G8UGQp0f0DrP60wRu9gfrH.jpg",
+        "adult": false,
+        "overview": "When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and Earth’s Mightiest Heroes are put to the ultimate test as the fate of the planet hangs in the balance. As the villainous Ultron emerges, it is up to The Avengers to stop him from enacting his terrible plans, and soon uneasy alliances and unexpected action pave the way for an epic and unique global adventure.",
+        "release_date": "2015-04-22",
+        "genre_ids": [
+          28,
+          12,
+          878
+        ],
+        "id": 99861,
+        "original_title": "Avengers: Age of Ultron",
+        "original_language": "en",
+        "backdrop_path": "/570qhjGZmGPrBGnfx70jcwIuBr4.jpg",
+        "popularity": 7.557812,
+        "vote_count": 3924,
+        "video": false,
+        "vote_average": 7.4
+      } )
     api = require("../../../../index");
   });
   afterEach((done) => {
@@ -117,7 +161,7 @@ describe("Movies endpoint", () => {
     describe("PUT /movies ", () => {
       it("should return a copy of the updated movie", () => {
         return request(api)
-          .put(`/api/movies/${movieId}`)
+          .put(`/api/movies/${currentMovieId}`)
           .send(movie)
           .expect(200)
           .then((res) => {
@@ -137,6 +181,33 @@ describe("Movies endpoint", () => {
               
             movieId = res.body.id;
           });
+      });
+    });
+    describe("Delete /movies/:id", () => {
+      describe("when the id is valid", () => {
+        it("should return a 200 status and confirmation message", () => {
+          return request(api)
+            .delete(`/api/movies/${currentMovieId}`)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .expect({
+              message: `Deleted movie id: ${currentMovieId}.`,
+              status: 200,
+            });
+        });
+        after(() => {
+          return request(api)
+            .get(`/api/movies/${currentMovieId}`)
+            .expect(404)
+            .expect({
+              message: `Unable to find movie with id: ${currentMovieId}.`,
+              status: 404,
+            });
+        });
+      });
+      describe("when the id is invalid", () => {
+        // TODO
       });
     });
 });
